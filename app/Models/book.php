4ee08bf5ea41ba7a76be56124a95a088c4ed5ad1
Farhan-Bintang <farbin00@gmail.com>
2022-12-16 16:08:30 +0700

    excel dan pdf

--- app/Models/book.php
+++ app/Models/book.php
@@ -10,6 +10,15 @@ use Illuminate\Support\Facades\Schema;
 
 class book extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'judul',
+        'penulis',
+        'tahun',
+        'penerbit'
+    ];
+
     public function up()
     {
         Schema::create('books', function(Blueprint $print){
@@ -21,6 +30,23 @@ class book extends Model
             $table->string('cover')->nullable();
             $table->timestamps();
         });
+    }
+
+    public static function getDatabooks()
+    {
+        $books = book::all();
+
+        $books_filter = [];
+        $no = 1;
+        for ($i=0; $i<$books->count(); $i++)
+        {
+            $books_filter[$i]['no'] = $no++;
+            $books_filter[$i]['judul'] = $books[$i]->judul;
+            $books_filter[$i]['penulis'] = $books[$i]->penulis;
+            $books_filter[$i]['tahun'] = $books[$i]->tahun;
+            $books_filter[$i]['penerbit'] = $books[$i]->penerbit;
+        }
+        return $books_filter;
     }
 
     public function down()
